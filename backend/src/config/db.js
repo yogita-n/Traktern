489f1c1e4f7d86e6b84c2c03e8a76db312386532
NehaBhask <nehabhaskar10b20@gmail.com>
2025-11-20 21:24:53 +0530

    Updated db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,2 +1,13 @@
-// DB Connection Placeholder
-// To be Implemented by Neha Bro
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("DB connection error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
